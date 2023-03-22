6333f199e6d9981747909b669fc499ae73e0b1af
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2023-03-22 14:00:06 +0300

    Изменил метод read_id

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ read_notes = ''
 
 def menu():
     print("Программа - Заметки")
-    print("Введите ___ 1 - создать ; 2 - сохранить ; 3 - читать ; 4 - редоктировать ; 5 - удалить ; 6 - выход")
+    print("Введите ___ 1 - создать ; 2 - сохранить ; 3 - читать ; 4 - читать по ID; 5 - редоктировать ; 6 - удалить ; 7 - выход")
     comand = input()
 
     if comand == '1':
@@ -16,17 +16,16 @@ def menu():
     elif comand == '3' and read_notes != '':
         read_notes(), menu()
     elif comand == '4':
-        edit_notes()
+        read_id_notes()
     elif comand == '5':
-        view_id()
+        edit_notes()
     elif comand == '6':
-        exit_notes()
+        dell_notes()
     elif comand == '7':
-        input()
+        exit_notes()
     else:
         error_notes()
     return comand
-
 
 def create_notes(notes):
     print(notes)
@@ -46,14 +45,20 @@ def save_notes(notes):
     except Exception:
         print("Создайте чтобы сохранить !!!"), menu()
 
+# def read_id_nots():
+#     with open('data.csv', 'r', encoding='utf_8') as file:
+#         return print(file.read())
+#
 def read_notes():
     menu_id = input('Весь список нажмите - 1 ; Заметка по id нажмите - 2  ')
     if menu_id == '1':
         with open('data.csv', 'r', encoding='utf_8') as file:
             return print(file.read())
     elif menu_id == '2':
-        temp_id = int(input('Введите номер ID ->   '))
+        temp = input('Введите номер ID ->   ')
         try:
+            arr = view_id()
+            temp_id = arr.index(temp)
             array = list()
             array1 = list()
             with open('data.csv', 'r', encoding='utf_8') as file:
@@ -61,13 +66,14 @@ def read_notes():
                 for i in array:
                     for j in i:
                         array1.append(j.split(';'))
-            temp_array = int(temp_id) - 1
-            print(array1)
+            temp_array = int(temp_id)
+            # print(array1)
             print(array1[temp_array])
         except Exception:
                         error_notes()
     else:
         read_notes()
+
 def edit_notes():
     try:
         temp_id = int(input('Enter id note ->  '))
@@ -83,21 +89,6 @@ def edit_notes():
         return print(*array1[temp_array]), print('Эти данные будут изменены !!! Введите новые данные : '), array1
     except Exception:
         error_notes()
-# def edit_notes():
-#     df = pd.read_csv('data.csv')
-#     df.loc[1, 'name'] = 'New text'
-#     df.to_csv('data.csv')
-
-# def edit_notes():
-#     for file in fileinput.input():
-#         print(file)
-#     # with open('data.txt', 'w', encoding='utf_8') as file:
-#     #     file_reader = csv.reader(file)
-#     with open('data1.csv', newline='', encoding='utf_8') as file:
-#         file_reader = csv.reader(file)
-#         for i in file_reader:
-#             print(', '.join(i))
-#             break
 
 def input_notes():
     array2 = list()
@@ -149,12 +140,14 @@ def view_id():
     for i in array1:
         for j in i:
             array2.append(j)
-    print(array2)
+    # print(array)
     array2 = array2[::4]
     print('Список id заметок: ')
-    return print(array2)
+    print(array2)
+    return array2
 
 
 if __name__ == '__main__':
     menu()
-    # add_notes(input_notes())
+    # add_notes(input_notes())5
+
